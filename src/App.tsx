5e55dca6bd3735902eb8e3cee166c1e2175020ba
Ilya Gusarov <i.gusarov86@gmail.com>
2020-10-11 21:23:59 +0400

    add layout components to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,29 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useLayoutEffect } from 'react';
 import styled from 'styled-components';
 import SendMessageForm from './components/SendMessageForm';
 import MessageList from './components/MessageList';
 import { useDispatch } from 'react-redux';
 import { addMessage } from './store/messages/actions';
 import { MessageType } from './store/messages/types';
+import { Container, Content, Footer, Header } from './components/Layout';
+import * as SocketIO from 'socket.io';
 
-declare const socket: any;
-
-const PageWrap = styled.div`
-  height: 100%;
-`;
-
-const AppWrap = styled.div`
-  height: 100%;
-  max-width: 400px;
-  margin: 0 auto;
-`;
-
-const AppContainer = styled.div`
-  position: relative;
-  height: 100%;
-`;
-
-const Header = styled.div`
-  position: absolute;
-  top: 0;
-  width: 100%;
-  z-index: 1;
-  height: 40px;
-  background-color: cornflowerblue;
-`;
-
-const Content = styled.div`
-  position: absolute;
-  width: 100%;
-  top: 0;
-  background: powderblue;
-  height: 100%;
-  padding: 40px 0 80px 0;
-`;
-
-const Footer = styled.div`
-  position: absolute;
-  bottom: 0;
-  height: 80px;
-  background: cornflowerblue;
-  width: 100%;
-`;
+declare const socket: SocketIO.Socket;
 
 export default function App() {
   const dispatch = useDispatch();
 
   useLayoutEffect(() => {
-    socket.on('connect', () => {
-      console.log('connected');
-      socket.on('message', handleNewMessage);
-    });
+    if (socket.connected) {
+      handleSocketConnected();
+    } else {
+      socket.on('connect', handleSocketConnected);
+    }
   }, []);
+
+  const handleSocketConnected = () => {
+    socket.on('message', handleNewMessage);
+  };
 
   const handleNewMessage = ({ text }: { text: string }) => {
     dispatch(
@@ -75,18 +41,14 @@ export default function App() {
   };
 
   return (
-    <PageWrap>
-      <AppWrap>
-        <AppContainer>
-          <Header>header</Header>
-          <Content>
-            <MessageList />
-          </Content>
-          <Footer>
-            <SendMessageForm onSubmit={handleSubmit} />
-          </Footer>
-        </AppContainer>
-      </AppWrap>
-    </PageWrap>
+    <Container>
+      <Header>header</Header>
+      <Content>
+        <MessageList />
+      </Content>
+      <Footer>
+        <SendMessageForm onSubmit={handleSubmit} />
+      </Footer>
+    </Container>
   );
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import React, { FC, ReactNode } from 'react';
+
+const PageWrap = styled.div`
+  height: 100%;
+`;
+
+const AppWrap = styled.div`
+  height: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+const AppContainer = styled.div`
+  position: relative;
+  height: 100%;
+`;
+
+export const Container: FC = ({ children }) => {
+  return (
+    <PageWrap>
+      <AppWrap>
+        <AppContainer>{children}</AppContainer>
+      </AppWrap>
+    </PageWrap>
+  );
+};
+
+export const Header = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+  height: 40px;
+  background-color: cornflowerblue;
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 0;
+  background: powderblue;
+  height: 100%;
+  padding: 40px 0 80px 0;
+`;
+
+export const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 80px;
+  background: cornflowerblue;
+  width: 100%;
+`;
