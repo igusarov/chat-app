8de8e8ca39bcef4ce3f59cf4b93b87c291cc4545
Ilya Gusarov <i.gusarov86@gmail.com>
2020-10-20 21:45:20 +0400

    add support from external devices

--- src/services/chatConnection.ts
+++ src/services/chatConnection.ts
@@ -1,12 +1,14 @@
 import io from 'socket.io-client';
 
+declare const location: Location;
+
 type MessagePayload = {
   userName: string;
   text: string;
 };
 
 class ChatConnection {
-  constructor(private socketIO = io('http://localhost:3000')) {}
+  constructor(private socketIO = io(`http://${location.hostname}:3000`)) {}
 
   public sendMessage(message: MessagePayload) {
     this.socket.emit('message', message);
